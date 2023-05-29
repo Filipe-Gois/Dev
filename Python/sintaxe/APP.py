e5f0f3f67d525ee1ed8b0d81907758f4283c4bf6
Filipe De Oliveira Góis <46633783825@info.local>
2023-05-29 17:19:04 -0300

    novo

---
+++ Python/sintaxe/APP.py
@@ -0,0 +1,12 @@
+import math
+nome = ''
+
+nome = input('\nOlá, insira seu nome:')
+
+print('\nInsira sua idade:')
+idade = input()
+
+
+print(f"\nSeu nome: {nome}. Sua idade é: {idade}")
+
+# print(math.pi, 2)
