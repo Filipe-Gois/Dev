437ad857095c9e4f245a374e4fdb3f82d4714f0a
Filipe Góis <fythoy@gmail.com>
2023-06-15 01:00:01 -0300

    Novo

--- JavaScript/teste/teste1.js
+++ JavaScript/teste/teste1.js
@@ -1,1 +1,11 @@
-console.log("Hello, wolrd");
+const readline = require('readline');
+const rl = readline.createInterface(
+    {
+        input: process.stdin,
+        output: process.stdout,
+    })
+
+    let nome
+
+rl.question('Qual o seu nome ?', nome)
+console.log(`OIOI, ${nome}`)
--- oprador ternário/JS/teste.js
+++ oprador ternário/JS/teste.js
@@ -1,4 +1,7 @@
 let txt1 = '1';
 let n1 = 1;
 
+
+
 txt1 == n1 ? console.log("Certo") : console.log("Errado")
+
