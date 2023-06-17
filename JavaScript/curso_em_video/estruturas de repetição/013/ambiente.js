2c79c643e3d046a723c727254e3a6fc624be7b9f
Filipe Góis <fythoy@gmail.com>
2023-06-17 18:55:39 -0300

    Novo

---
+++ JavaScript/curso_em_video/estruturas de repetição/013/ambiente.js
@@ -0,0 +1,32 @@
+// While:
+
+// var c = 1
+// while (c <= 6)
+// {   
+//     console.log(`${c}:oioioi`)
+//     c++
+// }
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+
+
+
+
+// Do while:
+
+// do {
+//     console.log(`${c}:oioioi`)
+//     c++
+// } while (c <= 6) 
+    
+
+// Laço FOR:
+console.log('Vai começar...')
+for (let index = 1; index <= 5; index++) { 
+    console.log(index)
+}
+
+console.log('fim!')
