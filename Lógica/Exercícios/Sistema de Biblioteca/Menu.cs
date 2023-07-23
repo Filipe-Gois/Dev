6656a77bd6de68378264410139401565185edf58
Filipe Góis <fythoy@gmail.com>
2023-07-22 21:54:59 -0300

    Novo

---
+++ Lógica/Exercícios/Sistema de Biblioteca/Menu.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Biblioteca
+{
+    public class Menu
+    {
+        
+    }
+}
