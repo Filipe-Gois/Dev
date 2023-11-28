438b6603b207f2b28a2a589d4c466fbe8ac6f914
Filipe Góis <fythoy@gmail.com>
2023-11-27 22:00:39 -0300

    novo

---
+++ Como enviar email/EnviaEmail/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EnviaEmail;
+
+Console.WriteLine("Enviando o email!");
+//SendEmail.Send("rowfegois@gmail.com");
+SendEmail.Send("pzm90260@nezid.com");
+Console.WriteLine($"Email enviado!!!");
---
+++ Como enviar email/EnviaEmail/SendEmail.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EnviaEmail
+{
+    public static class SendEmail
+    {
+        public static void Send(string email)
+        {
+            MailMessage emailMessage = new(); //variavel responsavel por criar/montar a mensagem -- informações da mensagem (quem envia/recebe, título, corpo)
+            try
+            {
+                var smtpClient = new SmtpClient("smtp.gmail.com", 587); //credenciais do servidor que enviará a mensagem
+                smtpClient.EnableSsl = true;
+                smtpClient.Timeout = 60 * 60;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential("fythoy@hotmail.com", ""); //passar o email e senha do remetente
+
+
+                emailMessage.From = new MailAddress("fythoy@gmail.com", "Filipe Góis"); //email e nome de quem está mandando
+                emailMessage.Body = "Testando o envio de email  smtp pelo GMAIL!"; //corpo do email
+                emailMessage.Subject = "Teste da WebApi do PicPay!";//título do email
+                emailMessage.IsBodyHtml = true; //"sim, o corpo do email será em html"
+                emailMessage.Priority = MailPriority.Normal;
+                emailMessage.To.Add(email); //destinatário da mensagem
+
+                smtpClient.Send(emailMessage);
+            }
+            catch (Exception e)
+            {
+
+                return;
+            }
+        }
+    }
+}
