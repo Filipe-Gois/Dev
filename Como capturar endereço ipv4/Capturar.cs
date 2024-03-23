475243bea77e03c52e1de998b6ea48413464ad44
Filipe Góis <fythiy@gmail.com>
2024-03-23 11:01:26 -0300

    novo

--- Como capturar endereço ipv4/Capturar.cs
+++ Como capturar endereço ipv4/Capturar.cs
@@ -12,3 +12,44 @@ static string GetIpv4()
     }
     throw new Exception("No network adapters with an IPv4 address in the system!");
 }
+
+
+
+//rascunho
+
+// using System;
+// using System.Net;
+// using System.Net.NetworkInformation;
+// using System.Net.Sockets;
+
+
+// static string GetIpv4()
+// {
+//     var host = Dns.GetHostEntry(Dns.GetHostName());
+
+
+//     foreach (var ip in host.AddressList)
+//     {
+//         if (ip == host.AddressList[6])
+//         {
+//             return ip.ToString();
+//         }
+//     }
+//     throw new Exception("No network adapters with an IPv4 address in the system!");
+// }
+
+// var host = Dns.GetHostEntry(Dns.GetHostName());
+
+// foreach (var ip in host.AddressList)
+// {
+//     if (ip == host.AddressList[6]) Console.WriteLine(ip);
+
+
+// }
+
+// Console.WriteLine(host.AddressList[0]);
+
+
+
+// Console.WriteLine(GetIpv4());
+// Console.WriteLine(Dns.GetHostEntry(Dns.GetHostName()).AddressList[3]);
