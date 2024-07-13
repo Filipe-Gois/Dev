cb76445513a884e902b4ffb256aa3535bfe8da3a
Filipe Góis <fythiy@gmail.com>
2024-07-13 01:35:43 -0300

    .

--- TypeScript/CFB cursos/src/p10/index.ts
+++ TypeScript/CFB cursos/src/p10/index.ts
@@ -6,6 +6,11 @@
 
 //unknown: tipo desconhecido. Só pode ser atribuido a variaveis do tipo unknown ou do tipo any
 
+// gpt:
+// null: Usado para indicar intencionalmente a ausência de valor. Tipicamente utilizado em React para retornar nada de um componente.
+// undefined: Indica que uma variável foi declarada mas não inicializada. Comum em estados e props que podem não ser fornecidos.
+// unknown: Usado para representar valores cujo tipo não é conhecido no momento da escrita do código. Exige verificação de tipo antes do uso.
+
 let vnome: string | null;
 vnome = "Filipe";
 console.log(vnome);
